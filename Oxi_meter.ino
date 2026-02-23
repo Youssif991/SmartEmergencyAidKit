08bec8d4f3e3d6ad1eb879215678fc6fafad9dcd
YoussefMohammed <youssif.sayed05@eng-st.cu.edu.eg>
2026-02-23 06:07:57 +0200

    Update Oxi_meter.ino

--- Oxi_meter.ino
+++ Oxi_meter.ino
@@ -7,19 +7,19 @@ MAX30105 particleSensor;
 
 #define MAX_BRIGHTNESS 255
 
-uint32_t irBuffer[100]; //Infrared LED sensor data
-uint32_t redBuffer[100];  //Red LED sensor data
+uint32_t irBuffer[100];
+uint32_t redBuffer[100];
 
-int32_t bufferLength; //Data length
-int32_t spo2; //SPO2 value
-int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
-int32_t heartRate; //heart rate value calcualated as per Maxim's algorithm
-int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+int32_t bufferLength;
+int32_t spo2;
+int8_t validSPO2;
+int32_t heartRate;
+int8_t validHeartRate;
 
-long lastBeat = 0; //Time at which the last beat occurred
+long lastBeat = 0;
 
-float beatsPerMinute; //stores the BPM as per custom algorithm
-int beatAvg = 0, sp02Avg = 0; //stores the average BPM and SPO2 
+float beatsPerMinute;
+int beatAvg = 0, sp02Avg = 0; 
 
 void setup()
 { 
@@ -29,14 +29,14 @@ void setup()
   
   Wire.begin(21, 22); //SDA = 21, SCL = 22
 
-  // Initialize sensor
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST))
   {
     Serial.println(F("MAX30105 was not found. Please check wiring/power."));
     while (1);
   }
 
-  /*The following parameters should be tuned to get the best readings for IR and RED LED. */
+  
   byte ledBrightness = 60;
   byte sampleAverage = 1;
   byte ledMode = 2;
@@ -61,32 +61,30 @@ void loop()
     particleSensor.nextSample();
   }
   
-  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+
   maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
   
-  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  
   while (1)
   {
-    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
     for (byte i = 25; i < 100; i++)
     {
       redBuffer[i - 25] = redBuffer[i];
       irBuffer[i - 25] = irBuffer[i];
     }
   
-    //take 25 sets of samples before calculating the heart rate.
     for (byte i = 75; i < 100; i++)
     {
-      while (particleSensor.available() == false) //do we have new data?
-        particleSensor.check(); //Check the sensor for new data
+      while (particleSensor.available() == false)
+        particleSensor.check();
     
       redBuffer[i] = particleSensor.getRed();
       irBuffer[i] = particleSensor.getIR();
-      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+      particleSensor.nextSample();
 
       long irValue = irBuffer[i];
 
-      //Calculate BPM independent of Maxim Algorithm. 
+       
       if (checkForBeat(irValue) == true)
       {
         //We sensed a beat!
@@ -96,17 +94,15 @@ void loop()
         beatsPerMinute = 60 / (delta / 1000.0);
         beatAvg = (beatAvg+beatsPerMinute)/2;
       }
-      if(millis() - lastBeat > 10000) // If no beat for 10 seconds, reset
+      if(millis() - lastBeat > 10000)
       {
         beatsPerMinute = 0;
         beatAvg = (beatAvg+beatsPerMinute)/2;
       }
     }
   
-    //After gathering 25 new samples recalculate HR and SP02
     maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
-    //Calculates average SPO2 to display smooth transitions 
     if(validSPO2 == 1 && spo2 < 100 && spo2 > 0)
     {
       sp02Avg = (sp02Avg+spo2)/2;
@@ -117,16 +113,13 @@ void loop()
       sp02Avg = (sp02Avg+spo2)/2;
     }
 
-    // --- NEW "NO FINGER" SAFETY CHECK ---
-    // If the last IR reading is less than 50,000, there is likely no finger on the sensor
     if (irBuffer[99] < 50000) {
       Serial.println("No finger detected. Waiting...");
-      beatAvg = 0; // Clear the bad data
-      sp02Avg = 0; // Clear the bad data
+      beatAvg = 0;
+      sp02Avg = 0;
     } 
     else 
     {
-      // --- NICE SERIAL MONITOR FORMATTING ---
       Serial.print("Heart Rate: ");
       if (validHeartRate && beatAvg > 20 && beatAvg < 200) { 
         Serial.print(beatAvg, DEC);
