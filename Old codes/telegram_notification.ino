47166c0d724a751a11f4c1871bbd8927d0d18118
abdel-dayem <Mahmoud.Dayem05@eng-st.cu.edu.eg>
2026-04-10 18:11:37 +0200

    Update telegram_notification.ino

--- Old codes/telegram_notification.ino
+++ Old codes/telegram_notification.ino
@@ -1,1 +1,104 @@
 
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define WIFI_SSID     "esm"       //matensa4 el wifi
+#define WIFI_PASSWORD "password"
+
+String botToken = "8685668389:AAFU7-nuRck-qb4Gr5FAcVvDwq13-dUBx7w";
+
+#define DATABASE_URL "https://health-71a04-default-rtdb.europe-west1.firebasedatabase.app"
+#define API_KEY       "AIzaSyC-ad77Wyw4_9FCQIbQwq53y-BRY02oe_o"
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+const int chatCount = 4;
+String chatIDs[chatCount] = {"5325723751", "1131440350", "1600412047", "1431425153"};
+
+bool telegramSent = false;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  
+  }
+
+  Serial.println("\nWiFi Connected!");
+}
+
+void loop() {
+
+  int hr   = 80;
+  float temp = 36;
+  int spo2 = 90;
+
+  sendToFirebase(hr, temp, spo2);
+  bool isCritical = (hr > 120 || hr < 50 || temp > 38.0 || spo2 < 90);
+
+  if (isCritical) {
+    
+ String alertMsg =
+        "🚨 EMERGENCY ALERT 🚨\nhelp the patient!\ncall 123!";
+
+  sendTelegram(alertMsg);
+  telegramSent = true;
+      
+  }
+  else {
+    telegramSent = false;
+  }
+
+}
+
+void sendToFirebase(int hr, float temp, int spo2) {
+
+  if (WiFi.status() != WL_CONNECTED) return;
+
+  HTTPClient http;
+
+  http.begin(String(DATABASE_URL) + "/health/hr.json");
+  http.PUT(String(hr));
+  http.end();
+
+  http.begin(String(DATABASE_URL) + "/health/temp.json");
+  http.PUT(String(temp));
+  http.end();
+
+  http.begin(String(DATABASE_URL) + "/health/spo2.json");
+  http.PUT(String(spo2));
+  http.end();
+ 
+}
+
+void sendTelegram(String message) {
+
+  if (WiFi.status() != WL_CONNECTED) return;
+
+  HTTPClient http;
+
+  message.replace(" ", "%20");
+  message.replace("\n", "%0A");
+
+ 
+  for (int i = 0; i < chatCount; i++) {
+
+    String url =
+      "https://api.telegram.org/bot" + botToken +
+      "/sendMessage?chat_id=" + chatIDs[i] +
+      "&text=" + message;
+
+    http.begin(url);
+    int httpCode = http.GET();
+    http.end();
+  } 
+}
